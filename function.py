6c73e7e863f1efdde79af61c7a95e9610c2ed28d
sianwatkins# <sianewatkins101@outlook.com>
2020-12-15 12:30:08 +0000

    new commits

--- function.py
+++
@@ -1,7 +0,0 @@
-***functions
-hotel = format_table(hotel)
-new_hotel = delete_cancelled(hotel)
-user_date = user_interface()
-hotel2 = select_7days(user_date, new_hotel)
-new = group(user_date)
-file = file()
