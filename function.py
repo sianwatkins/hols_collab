865fa89749dbf2ad1c4cbb6110095d9410ca89d0
sianwatkins# <sianewatkins101@outlook.com>
2020-12-15 11:48:08 +0000

    functions sheet

--- function.py
+++ function.py
@@ -1,2 +1,7 @@
-functions
-
+***functions
+hotel = format_table(hotel)
+new_hotel = delete_cancelled(hotel)
+user_date = user_interface()
+hotel2 = select_7days(user_date, new_hotel)
+new = group(user_date)
+file = file()
