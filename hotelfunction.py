84bde4f77dd0af9212aec54e74e49659f71490be
sianwatkins# <sianewatkins101@outlook.com>
2020-12-15 12:29:42 +0000

    changes to function sheet

--- hotelfunction.py
+++ hotelfunction.py
@@ -5,10 +5,11 @@ import tkinter as tk
 from tkinter import filedialog
 
 def hotelbooking():
-    file
-    format_table(hotel)
-    delete_cancelled(hotel)
-    user_interface
-    select_7days(user_date, new_hotel)
-    group(user_date)
+    hotel = format_table(hotel)
+    new_hotel = delete_cancelled(hotel)
+    user_date = user_interface()
+    hotel2 = select_7days(user_date, new_hotel)
+    new = group(user_date)
+    file = file()
 
+hotel_booking()
